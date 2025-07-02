ad65f89103a3b55323d4b7b59ef824304cbf967d
Firestar99 <firestar99@sydow.cloud>
2025-07-02 19:35:46 +0200

    move camera setup to `scene_setup`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use crate::camera_controller::{CameraController, CameraControllerPlugin};
+use crate::camera_controller::CameraControllerPlugin;
 use bevy::{pbr::ExtendedMaterial, prelude::*};
-use bevy::core_pipeline::Skybox;
 use custom_shaders::*;
 
 mod custom_shaders;
@@ -9,13 +8,14 @@ mod base_plane;
 mod camera_controller;
 pub mod double_sided;
 mod extended_material;
+mod scene_setup;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(CameraControllerPlugin)
-        .add_systems(Startup, setup)
-        .add_systems(Update, rotate_things)
+        .add_systems(Startup, scene_setup::setup)
+        .add_systems(Update, scene_setup::rotate_things)
         .add_systems(Startup, base_plane::setup)
         .add_plugins(MaterialPlugin::<a_basic_color::CustomMaterial>::default())
         .add_systems(Startup, a_basic_color::setup)
@@ -46,50 +46,4 @@ fn main() {
         >::default())
         .add_systems(Startup, extended_material::setup)
         .run();
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // light
-    commands.spawn((
-        DirectionalLight::default(),
-        Transform::from_xyz(1.0, 1.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
-        Rotate,
-    ));
-
-    // camera
-    commands.spawn((
-        Camera3d::default(),
-        Projection::Perspective(PerspectiveProjection {
-            fov: 70.,
-            ..Default::default()
-        }),
-        CameraController {
-            key_down: KeyCode::ShiftLeft,
-            key_up: KeyCode::Space,
-            key_run: KeyCode::ControlLeft,
-            scroll_factor: 0.2,
-            ..CameraController::default()
-        },
-        Skybox {
-            brightness: 2000.0,
-            image: asset_server.load("textures/hdr/pisa_specular_rgb9e5_zstd.ktx2"),
-            ..default()
-        },
-        EnvironmentMapLight {
-            diffuse_map: asset_server.load("textures/hdr/pisa_diffuse_rgb9e5_zstd.ktx2"),
-            specular_map: asset_server.load("textures/hdr/pisa_specular_rgb9e5_zstd.ktx2"),
-            intensity: 900.0,
-            ..default()
-        },
-        Transform::from_translation(Vec3::new(-2.0, 2.5, 5.0) / 3.).looking_at(Vec3::ZERO, Vec3::Y),
-    ));
-}
-
-#[derive(Component)]
-struct Rotate;
-
-fn rotate_things(mut q: Query<&mut Transform, With<Rotate>>, time: Res<Time>) {
-    for mut t in &mut q {
-        t.rotate_y(time.delta_secs());
-    }
 }
---
+++ src/scene_setup.rs
@@ -0,0 +1,55 @@
+use crate::camera_controller::CameraController;
+use bevy::asset::AssetServer;
+use bevy::core_pipeline::Skybox;
+use bevy::math::Vec3;
+use bevy::pbr::DirectionalLight;
+use bevy::prelude::{
+    Camera3d, Commands, Component, EnvironmentMapLight, KeyCode, PerspectiveProjection, Projection,
+    Query, Res, Time, Transform, With, default,
+};
+
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // light
+    commands.spawn((
+        DirectionalLight::default(),
+        Transform::from_xyz(1.0, 1.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Rotate,
+    ));
+
+    // camera
+    commands.spawn((
+        Camera3d::default(),
+        Projection::Perspective(PerspectiveProjection {
+            fov: 70.,
+            ..Default::default()
+        }),
+        CameraController {
+            key_down: KeyCode::ShiftLeft,
+            key_up: KeyCode::Space,
+            key_run: KeyCode::ControlLeft,
+            scroll_factor: 0.2,
+            ..CameraController::default()
+        },
+        Skybox {
+            brightness: 2000.0,
+            image: asset_server.load("textures/hdr/pisa_specular_rgb9e5_zstd.ktx2"),
+            ..default()
+        },
+        EnvironmentMapLight {
+            diffuse_map: asset_server.load("textures/hdr/pisa_diffuse_rgb9e5_zstd.ktx2"),
+            specular_map: asset_server.load("textures/hdr/pisa_specular_rgb9e5_zstd.ktx2"),
+            intensity: 900.0,
+            ..default()
+        },
+        Transform::from_translation(Vec3::new(-2.0, 2.5, 5.0) / 3.).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
+
+#[derive(Component)]
+pub struct Rotate;
+
+pub fn rotate_things(mut q: Query<&mut Transform, With<Rotate>>, time: Res<Time>) {
+    for mut t in &mut q {
+        t.rotate_y(time.delta_secs());
+    }
+}
